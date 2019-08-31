3c5765345c97a3a8bd9968afd39a6629bb3b5403
inioluwa adebanjo <adeb6600@gmail.com>
2019-08-31 15:19:24 +0100

    add dateexpiry to document addition

--- modules/account/schemas.js
+++ modules/account/schemas.js
@@ -1,328 +1,332 @@
-const requestProperties ={
-    apiKey: {
-      type: 'string'
-    },
-    randomguid: {
-      type: 'string'
-    },
-      hash: {
+const requestProperties = {
+  apiKey: {
+    type: 'string'
+  },
+  randomguid: {
+    type: 'string'
+  },
+  hash: {
+    type: 'string'
+  },
+
+}
+const user = {
+  // This jsonschema will be used for data validation
+  body: {
+    type: 'object',
+    properties: {
+      uid: {
         type: 'string'
       },
-  
-  }
-const user = {
-    // This jsonschema will be used for data validation
-    body: {
-      type: 'object',
-      properties: {
-        uid: {
-          type: 'string'
-        },
-          sessiontoken: {
-            type: 'string'
-          },
-      } ,
-      additionalProperties: false
+      sessiontoken: {
+        type: 'string'
+      },
     },
-    response: {
-      // The 200 body response is described
-      // by the following schema
-      200: {
-        type: 'object',
-        required: [ 'message' ],
-        properties: {
-          message: { type: 'string' },
-          uid: {type:'string'},
-          uno: {type:'string'},
-          lastloggedin: {type:'string'},
-          firstname: {type:'string'},
-          lastname: {type:'string'},
-          email: {type:'string'},
-          country: {type:'string'},
-          postcode:{type:'string'},
-          countrycode: {type:'string'},
-          balances: {type:'array',
-        items: {
-            type:'object',
-            properties : {
-      currencycode:{ type: 'string' },
-       amount:{ type: 'number' },
-         }
-        } },
-          gender: {type:'string'},
-          mobileno: {type:'string'},
-          dob: {type:'string'},
-        },
-        additionalProperties: false
-      }
-    }
-  }
-  
-  const corridors = {
-    // This jsonschema will be used for data validation
-    // body: {
-    //   type: 'object',
-    //   properties: requestProperties ,
-    //   additionalProperties: false
-    // },
-    response: {
-      // The 200 body response is described
-      // by the following schema
-      200: {
-        type: 'object',
-        required: [ 'message' ],
-        properties: {
-          message: { type: 'string' },
-          count: {type:'number'},
-          corridors: {type:'array',
-        items: {
-            type:'object',
-            properties : {
-      CorridorCode:{ type: 'string' },
-       FromCountryISO3:{ type: 'string' },
-       FromCountryName:{ type: 'string' },
-       fromCurrencyISO3:{ type: 'string' },
-       ToCountryISO3 :{ type: 'string' },
-       TOCountryName :{ type: 'string' },
-       ToCurrencyISO3 :{ type: 'string' }, 
-       IsCanReceiveAirtime:{ type: 'boolean' },
-       IsCanReceiveBillPay:{ type: 'boolean' },
-       IsCanReceiveMoneyBank:{ type: 'boolean' },
-       IsCanReceiveMoneyCash:{ type: 'boolean' },
-       IsCanReceiveMoneyMobile:{ type: 'boolean' },
-       MinSendValue: {type:'number'},
-       MaxSendValue: {type:'number'},
-       MinReceiveValue: {type:'number'},
-       MaxReceiveValue: {type:'number'},
-       
-            }
-        } }
-        },
-        additionalProperties: false
-      }
-    }
-  }
-
-  const beneficiaries = {
-    // This jsonschema will be used for data validation
-    // body: {
-    //   type: 'object',
-    //   properties: requestProperties ,
-    //   additionalProperties: false
-    // },
-    response: {
-      // The 200 body response is described
-      // by the following schema
-      200: {
-        type: 'object',
-        required: [ 'message' ],
-        properties: {
-          message: { type: 'string' },
-          firstname: {type:'string'},
-          lastname: {type:'string'},
-          country: {type:'string'},
-          currency: {type:'string'},
-          servicecode: {type:'string'},
-          reasonid: {type:'string'},
-          reason: {type:'string'},
-          relationshipid: {type:'string'},
-          relationship: {type:'string'},
-          providerid: {type:'string'},
-          provider:{type:'string'},
-          provideritemid: {type:'string'},
-          provideritem: {type:'string'},
-          reference : {type:'string'},
-          ibank: {type:'string'},
-          swift: {type:'string'},
-          routing: {type:'string'},
-          date: {type:'string'},
-          active:{type:'boolean'}
-        },
-        additionalProperties: false
-      }
-    }
-  }
-  const beneficiary = {
-    // This jsonschema will be used for data validation
-    // body: {
-    //   type: 'object',
-    //   properties: requestProperties ,
-    //   additionalProperties: false
-    // },
-    response: {
-      // The 200 body response is described
-      // by the following schema
-      200: {
-        type: 'object',
-        required: [ 'message' ],
-        properties: {
-          message: { type: 'string' },
-          firstname: {type:'string'},
-          lastname: {type:'string'},
-          country: {type:'string'},
-          currency: {type:'string'},
-          servicecode: {type:'string'},
-          reasonid: {type:'string'},
-          reason: {type:'string'},
-          relationshipid: {type:'string'},
-          relationship: {type:'string'},
-          providerid: {type:'string'},
-          provider:{type:'string'},
-          provideritemid: {type:'string'},
-          provideritem: {type:'string'},
-          reference : {type:'string'},
-          ibank: {type:'string'},
-          swift: {type:'string'},
-          routing: {type:'string'},
-          date: {type:'string'},
-          active:{type:'boolean'}
-        },
-        additionalProperties: false
-      }
-    }
-  }
-  const createbeneficiary = {
-    // This jsonschema will be used for data validation
-    body: {
+    additionalProperties: false
+  },
+  response: {
+    // The 200 body response is described
+    // by the following schema
+    200: {
       type: 'object',
-      required:[
-        'uid','sessiontoken','firstname','lastname','active','deleted',
-        'service','country','currency','provider',
-        'account'
-      ],
+      required: ['message'],
       properties: {
+        message: { type: 'string' },
         uid: { type: 'string' },
-        sessiontoken: { type: 'string' },
-        contact:{type:'string'},
-         firstname: {type:'string'},
-          lastname: {type:'string'},
-          account: {type:'string'},
-          active: {type:'integer'},
-          deleted: {type:'integer'},
-          service: {type:'string'},
-          country: {type:'string'},
-          currency: {type:'string'},
-          reason: {type:'string'},
-          relationship: {type:'string'},
-          provider: {type:'string'},
-          provideritem: {type:'string'},
-          reference: {type:'string'},
-          iban: {type:'string'},
-          swift: {type:'string'},
-          routing: {type:'string'},
-          providername: {type:'string'}
-      } ,
-
-      additionalProperties: false
-    },
-    response: {
-      // The 200 body response is described
-      // by the following schema
-      200: {
-        type: 'object',
-        required: [ 'message' ],
-        properties: {
-          message: { type: 'string' },
-          
-        },
-        additionalProperties: false
-      }
-    }
-  } 
-  const listbeneficiary = {
-    // This jsonschema will be used for data validation
-    body: {
-      type: 'object',
-      required:[
-        'uid','sessiontoken'
-      ],
-      properties: {
-        uid: { type: 'string' },
-        sessiontoken: { type: 'string' },
-          service: {type:'string'},
-          country: {type:'string'}
-         } ,
-
-      additionalProperties: false
-    },
-    response: {
-      // The 200 body response is described
-      // by the following schema
-      200: {
-        type: 'object',
-        required: [ 'message' ],
-        properties: {
-          message: { type: 'string' },
-          contacts:{
-            type:'array',
-            items:{
-              type:'object',
-              properties: {
-                id:{ type: 'string' },
-                name:{ type: 'string' },
-                country:{ type: 'string' },
-                countrycode:{ type: 'string' },
-                service:{ type: 'string' },
-                servicecode:{ type: 'string' },
-                datecreated:{ type: 'string' },
-                active:{ type: 'string' },
-                reference:{ type: 'string' },
-                currency:{ type: 'string' },
-                firstname:{ type: 'string' },
-                lastname:{ type: 'string' },
-                iban:{ type: 'string' },
-                routing:{ type: 'string' },
-                provider:{ type: 'string' },
-                swift:{ type: 'string' },
-                relationship:{ type: 'string' },
-                reason:{ type: 'string' }
-          
-              }
+        uno: { type: 'string' },
+        lastloggedin: { type: 'string' },
+        firstname: { type: 'string' },
+        lastname: { type: 'string' },
+        email: { type: 'string' },
+        country: { type: 'string' },
+        postcode: { type: 'string' },
+        countrycode: { type: 'string' },
+        balances: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              currencycode: { type: 'string' },
+              amount: { type: 'number' },
             }
           }
-          
         },
-        additionalProperties: false
-      }
-    }
-  } 
-
-  const document = {
-    // This jsonschema will be used for data validation
-    body: {
-      type: 'object',
-      required:[
-        'uid','sessiontoken','title','typeid','docstring'
-      ],
-      properties: {
-        uid: { type: 'string' },
-        sessiontoken: { type: 'string' },
-        title:{type:'string'},
-         typeid: {type:'string'},
-          docstring: {type:'string'},
-      } ,
-
+        gender: { type: 'string' },
+        mobileno: { type: 'string' },
+        dob: { type: 'string' },
+      },
       additionalProperties: false
-    },
-    response: {
-      // The 200 body response is described
-      // by the following schema
-      200: {
-        type: 'object',
-        required: [ 'message' ],
-        properties: {
-          message: { type: 'string' },
-          docid:{type:'string'}
-        },
-        additionalProperties: false
-      }
     }
-  } 
-  module.exports =  { 
-      user,
-      createbeneficiary,
-      listbeneficiary,
-      document
   }
+}
 
-  
+const corridors = {
+  // This jsonschema will be used for data validation
+  // body: {
+  //   type: 'object',
+  //   properties: requestProperties ,
+  //   additionalProperties: false
+  // },
+  response: {
+    // The 200 body response is described
+    // by the following schema
+    200: {
+      type: 'object',
+      required: ['message'],
+      properties: {
+        message: { type: 'string' },
+        count: { type: 'number' },
+        corridors: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              CorridorCode: { type: 'string' },
+              FromCountryISO3: { type: 'string' },
+              FromCountryName: { type: 'string' },
+              fromCurrencyISO3: { type: 'string' },
+              ToCountryISO3: { type: 'string' },
+              TOCountryName: { type: 'string' },
+              ToCurrencyISO3: { type: 'string' },
+              IsCanReceiveAirtime: { type: 'boolean' },
+              IsCanReceiveBillPay: { type: 'boolean' },
+              IsCanReceiveMoneyBank: { type: 'boolean' },
+              IsCanReceiveMoneyCash: { type: 'boolean' },
+              IsCanReceiveMoneyMobile: { type: 'boolean' },
+              MinSendValue: { type: 'number' },
+              MaxSendValue: { type: 'number' },
+              MinReceiveValue: { type: 'number' },
+              MaxReceiveValue: { type: 'number' },
+
+            }
+          }
+        }
+      },
+      additionalProperties: false
+    }
+  }
+}
+
+const beneficiaries = {
+  // This jsonschema will be used for data validation
+  // body: {
+  //   type: 'object',
+  //   properties: requestProperties ,
+  //   additionalProperties: false
+  // },
+  response: {
+    // The 200 body response is described
+    // by the following schema
+    200: {
+      type: 'object',
+      required: ['message'],
+      properties: {
+        message: { type: 'string' },
+        firstname: { type: 'string' },
+        lastname: { type: 'string' },
+        country: { type: 'string' },
+        currency: { type: 'string' },
+        servicecode: { type: 'string' },
+        reasonid: { type: 'string' },
+        reason: { type: 'string' },
+        relationshipid: { type: 'string' },
+        relationship: { type: 'string' },
+        providerid: { type: 'string' },
+        provider: { type: 'string' },
+        provideritemid: { type: 'string' },
+        provideritem: { type: 'string' },
+        reference: { type: 'string' },
+        ibank: { type: 'string' },
+        swift: { type: 'string' },
+        routing: { type: 'string' },
+        date: { type: 'string' },
+        active: { type: 'boolean' }
+      },
+      additionalProperties: false
+    }
+  }
+}
+const beneficiary = {
+  // This jsonschema will be used for data validation
+  // body: {
+  //   type: 'object',
+  //   properties: requestProperties ,
+  //   additionalProperties: false
+  // },
+  response: {
+    // The 200 body response is described
+    // by the following schema
+    200: {
+      type: 'object',
+      required: ['message'],
+      properties: {
+        message: { type: 'string' },
+        firstname: { type: 'string' },
+        lastname: { type: 'string' },
+        country: { type: 'string' },
+        currency: { type: 'string' },
+        servicecode: { type: 'string' },
+        reasonid: { type: 'string' },
+        reason: { type: 'string' },
+        relationshipid: { type: 'string' },
+        relationship: { type: 'string' },
+        providerid: { type: 'string' },
+        provider: { type: 'string' },
+        provideritemid: { type: 'string' },
+        provideritem: { type: 'string' },
+        reference: { type: 'string' },
+        ibank: { type: 'string' },
+        swift: { type: 'string' },
+        routing: { type: 'string' },
+        date: { type: 'string' },
+        active: { type: 'boolean' }
+      },
+      additionalProperties: false
+    }
+  }
+}
+const createbeneficiary = {
+  // This jsonschema will be used for data validation
+  body: {
+    type: 'object',
+    required: [
+      'uid', 'sessiontoken', 'firstname', 'lastname', 'active', 'deleted',
+      'service', 'country', 'currency', 'provider',
+      'account'
+    ],
+    properties: {
+      uid: { type: 'string' },
+      sessiontoken: { type: 'string' },
+      contact: { type: 'string' },
+      firstname: { type: 'string' },
+      lastname: { type: 'string' },
+      account: { type: 'string' },
+      active: { type: 'integer' },
+      deleted: { type: 'integer' },
+      service: { type: 'string' },
+      country: { type: 'string' },
+      currency: { type: 'string' },
+      reason: { type: 'string' },
+      relationship: { type: 'string' },
+      provider: { type: 'string' },
+      provideritem: { type: 'string' },
+      reference: { type: 'string' },
+      iban: { type: 'string' },
+      swift: { type: 'string' },
+      routing: { type: 'string' },
+      providername: { type: 'string' }
+    },
+
+    additionalProperties: false
+  },
+  response: {
+    // The 200 body response is described
+    // by the following schema
+    200: {
+      type: 'object',
+      required: ['message'],
+      properties: {
+        message: { type: 'string' },
+
+      },
+      additionalProperties: false
+    }
+  }
+}
+const listbeneficiary = {
+  // This jsonschema will be used for data validation
+  body: {
+    type: 'object',
+    required: [
+      'uid', 'sessiontoken'
+    ],
+    properties: {
+      uid: { type: 'string' },
+      sessiontoken: { type: 'string' },
+      service: { type: 'string' },
+      country: { type: 'string' }
+    },
+
+    additionalProperties: false
+  },
+  response: {
+    // The 200 body response is described
+    // by the following schema
+    200: {
+      type: 'object',
+      required: ['message'],
+      properties: {
+        message: { type: 'string' },
+        contacts: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              id: { type: 'string' },
+              name: { type: 'string' },
+              country: { type: 'string' },
+              countrycode: { type: 'string' },
+              service: { type: 'string' },
+              servicecode: { type: 'string' },
+              datecreated: { type: 'string' },
+              active: { type: 'string' },
+              reference: { type: 'string' },
+              currency: { type: 'string' },
+              firstname: { type: 'string' },
+              lastname: { type: 'string' },
+              iban: { type: 'string' },
+              routing: { type: 'string' },
+              provider: { type: 'string' },
+              swift: { type: 'string' },
+              relationship: { type: 'string' },
+              reason: { type: 'string' }
+
+            }
+          }
+        }
+
+      },
+      additionalProperties: false
+    }
+  }
+}
+
+const document = {
+  // This jsonschema will be used for data validation
+  body: {
+    type: 'object',
+    required: [
+      'uid', 'sessiontoken', 'dateexpire', 'typeid', 'docstring'
+    ],
+    properties: {
+      uid: { type: 'string' },
+      sessiontoken: { type: 'string' },
+      dateexpire: { type: 'string' },
+      title: { type: 'string' },
+      typeid: { type: 'string' },
+      docstring: { type: 'string' },
+    },
+
+    additionalProperties: false
+  },
+  response: {
+    // The 200 body response is described
+    // by the following schema
+    200: {
+      type: 'object',
+      required: ['message'],
+      properties: {
+        message: { type: 'string' },
+        docid: { type: 'string' }
+      },
+      additionalProperties: false
+    }
+  }
+}
+module.exports = {
+  user,
+  createbeneficiary,
+  listbeneficiary,
+  document
+}
+
--- modules/account/service.js
+++ modules/account/service.js
@@ -1,27 +1,27 @@
-const  {R , hasher}  =  require('../../lib/api')
+const { R, hasher } = require('../../lib/api')
 const uuid = require('uuid/v4')
 const to = require('await-to-js').default;
-class  AccountService { 
-  
-    async useraccount (sessiontoken,uid) { 
+class AccountService {
+
+    async useraccount(sessiontoken, uid) {
         let userResult;
         const randomguid = uuid()
 
-        const hash = hasher(randomguid,process.env.PRIVATE_KEY, process.env.API_KEY, sessiontoken,uid)
-      
-            userResult = await R.post('/RetailAccountUserGet', {
-                randomguid,
-                apiKey: process.env.API_KEY, 
-                hash,
-                uid,
-                sessiontoken
-            })
-       
+        const hash = hasher(randomguid, process.env.PRIVATE_KEY, process.env.API_KEY, sessiontoken, uid)
+
+        userResult = await R.post('/RetailAccountUserGet', {
+            randomguid,
+            apiKey: process.env.API_KEY,
+            hash,
+            uid,
+            sessiontoken
+        })
+
         return userResult.data.RetailApiResponse
-    
+
     }
 
-    async createBeneficiary ({sessiontoken, uid, 
+    async createBeneficiary({ sessiontoken, uid,
         contact,
         firstname,
         lastname,
@@ -30,130 +30,135 @@ class  AccountService {
         service,
         country,
         currency,
-       reason,
-       relationship,
-         provider,
-       provideritem,
+        reason,
+        relationship,
+        provider,
+        provideritem,
         providername,
         account,
         iban,
         swift,
         routing
-}) { 
+    }) {
         let userResult;
         let userRequest = {
-           fname: firstname,
-            lname:lastname,
-            isactive:active,
+            fname: firstname,
+            lname: lastname,
+            isactive: active,
             isdelete: deleted,
-            countryiso3:country,
-            currencyiso:currency,
-            servicecode:service,
+            countryiso3: country,
+            currencyiso: currency,
+            servicecode: service,
             providerid: provider,
-           providername,
-           accountref:account,
+            providername,
+            accountref: account,
         }
-        if(contact) userRequest['contactid'] = contact
-        if(relationship) userRequest['relationshipid'] = relationship
-        if(reason) userRequest['reasonid'] = reason
-      
-        if(iban) userRequest['iban'] = iban
-        if(swift) userRequest['swift'] = swift
-        if(routing) userRequest['routing'] = routing
-        if(provideritem) userRequest['provideritemid'] = provideritem
+        if (contact) userRequest['contactid'] = contact
+        if (relationship) userRequest['relationshipid'] = relationship
+        if (reason) userRequest['reasonid'] = reason
+
+        if (iban) userRequest['iban'] = iban
+        if (swift) userRequest['swift'] = swift
+        if (routing) userRequest['routing'] = routing
+        if (provideritem) userRequest['provideritemid'] = provideritem
         const randomguid = uuid()
-        const hash =contact ? hasher(randomguid,process.env.PRIVATE_KEY, process.env.API_KEY, sessiontoken,uid,contact):
-        hasher(randomguid,process.env.PRIVATE_KEY, process.env.API_KEY, sessiontoken,uid)
-            userResult = await R.post('/RetailAccountBeneficiarySave', {
-                randomguid,
-                apiKey: process.env.API_KEY, 
-                hash,
-                uid, 
-                sessiontoken,
-                ...userRequest
-            })
+        const hash = contact ? hasher(randomguid, process.env.PRIVATE_KEY, process.env.API_KEY, sessiontoken, uid, contact) :
+            hasher(randomguid, process.env.PRIVATE_KEY, process.env.API_KEY, sessiontoken, uid)
+        userResult = await R.post('/RetailAccountBeneficiarySave', {
+            randomguid,
+            apiKey: process.env.API_KEY,
+            hash,
+            uid,
+            sessiontoken,
+            ...userRequest
+        })
         return userResult.data.RetailApiResponse
-    
+
     }
 
-    async listBeneficiary ({sessiontoken, uid, 
+    async listBeneficiary({ sessiontoken, uid,
         service,
         country,
-}) {
-    let recipientListQuery = [];
-   
+    }) {
+        let recipientListQuery = [];
+
         let userRequest = {
-            countryiso3:country,
+            countryiso3: country,
             service,
-             }
-             if(country) userRequest['countryiso3'] = country
-             if(service) userRequest['service'] = service
-      
+        }
+        if (country) userRequest['countryiso3'] = country
+        if (service) userRequest['service'] = service
+
         const randomguid = uuid()
-        const hash = hasher(randomguid,process.env.PRIVATE_KEY, process.env.API_KEY, sessiontoken,uid,country)
-     
-        const [error,userResult] = await to(R.post('/RetailAccountBeneficiaryList', {
-                randomguid,
-                apiKey: process.env.API_KEY, 
-                hash,
-                uid, 
-                sessiontoken,
-                ...userRequest
-            }));
-           
-     //    return userResult.data.RetailApiResponse
-      
-    if(error) return error
-    if(!userResult.data.RetailApiResponse.Contacts.length) {
-        return [];
-    }
-    userResult.data.RetailApiResponse.Contacts.map(cnt=>{
-       let cuuid = uuid();  
-        let Chash = hasher(cuuid,process.env.PRIVATE_KEY, process.env.API_KEY,sessiontoken,uid, cnt.ContactId)
-        let Creq = R.post('/RetailAccountBeneficiaryGet', {
-            randomguid:cuuid,
-            apiKey: process.env.API_KEY, 
-            hash:Chash,
-            contactid: cnt.ContactId,
-            sessiontoken,
-            Uid:uid,
-        })
-        recipientListQuery.push(Creq)
-    })
-    const recipientGetResult =  await Promise.all(recipientListQuery);
-    const recipientGetResultArray =  recipientGetResult.filter(c=>c.data.RetailApiResponse.ResponseCode === '10000').map(cntGet=>{
+        const hash = hasher(randomguid, process.env.PRIVATE_KEY, process.env.API_KEY, sessiontoken, uid, country)
 
-        return cntGet.data.RetailApiResponse.Contacts[0]
-    })
-   
-    return recipientGetResultArray
-
-}
-
-async addDocument ({sessiontoken, uid, 
-    title,
-    typeid,
-    docstring
-}) { 
-    let documentResult;
-    let documentRequest = {
-        title,
-        format:'jpg',
-        typeid,
-        docbase64string:docstring
-    }
-    const randomguid = uuid();
-    const hash = hasher(randomguid,process.env.PRIVATE_KEY, process.env.API_KEY, sessiontoken,uid)   
-    documentResult = await R.post('/RetailAccountDocumentAdd', {
+        const [error, userResult] = await to(R.post('/RetailAccountBeneficiaryList', {
             randomguid,
-            apiKey: process.env.API_KEY, 
+            apiKey: process.env.API_KEY,
             hash,
-            uid, 
+            uid,
+            sessiontoken,
+            ...userRequest
+        }));
+
+        //    return userResult.data.RetailApiResponse
+
+        if (error) return error
+        if (!userResult.data.RetailApiResponse.Contacts.length) {
+            return [];
+        }
+        userResult.data.RetailApiResponse.Contacts.map(cnt => {
+            let cuuid = uuid();
+            let Chash = hasher(cuuid, process.env.PRIVATE_KEY, process.env.API_KEY, sessiontoken, uid, cnt.ContactId)
+            let Creq = R.post('/RetailAccountBeneficiaryGet', {
+                randomguid: cuuid,
+                apiKey: process.env.API_KEY,
+                hash: Chash,
+                contactid: cnt.ContactId,
+                sessiontoken,
+                Uid: uid,
+            })
+            recipientListQuery.push(Creq)
+        })
+        const recipientGetResult = await Promise.all(recipientListQuery);
+        const recipientGetResultArray = recipientGetResult.filter(c => c.data.RetailApiResponse.ResponseCode === '10000').map(cntGet => {
+
+            return cntGet.data.RetailApiResponse.Contacts[0]
+        })
+
+        return recipientGetResultArray
+
+    }
+
+    async addDocument({ sessiontoken, uid,
+        title,
+        dateexpire,
+        typeid,
+        docstring
+    }) {
+        let documentResult;
+        let documentRequest = {
+            title,
+            format: 'jpg',
+            typeid,
+            dateexpire,
+            docbase64string: docstring
+        }
+        if (title) {
+            documentRequest['title'] = title;
+        }
+        const randomguid = uuid();
+        const hash = hasher(randomguid, process.env.PRIVATE_KEY, process.env.API_KEY, sessiontoken, uid)
+        documentResult = await R.post('/RetailAccountDocumentAdd', {
+            randomguid,
+            apiKey: process.env.API_KEY,
+            hash,
+            uid,
             sessiontoken,
             ...documentRequest
         })
-    return documentResult.data.RetailApiResponse
+        return documentResult.data.RetailApiResponse
 
-}
+    }
 }
 module.exports = AccountService
