75ac0f66b1b2d6e2de96eed9fe93071ffccb9bd3
inioluwa adebanjo <adeb6600@gmail.com>
2020-03-02 07:27:27 +0100

    log response

--- modules/transaction/service.js
+++ modules/transaction/service.js
@@ -38,6 +38,7 @@ class  TransactionService {
                 isvalidate
 
             })
+            console.log('calculate response', calculateResult.data.RetailApiResponse)
            return calculateResult.data.RetailApiResponse
 
     }
