10245423e2355f40fffc50408a59b5572fe4fb2f
inioluwa adebanjo <adeb6600@gmail.com>
2020-08-26 06:06:06 +0100

    log bank requirements

--- modules/transaction/index.js
+++ modules/transaction/index.js
@@ -270,7 +270,11 @@ module.exports[Symbol.for('plugin-meta')] = {
         value:calculate.TransactionCalculate.DataSenderIdCountry
       })
     }
+    console.log('doc requirement', docrequirements);
+    console.log('data requirement', datarequirements);
+    console.log('kyc requirement', kycrequirements);
     console.log('bank requirement', bankrequirements);
+    console.log('sender requirement', senderrequirements);
     return {
         message: calculate.ResponseMessage,
         rate: calculate.TransactionCalculate.Rate,
