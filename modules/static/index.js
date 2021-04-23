79cf85b6ff4a14f7301d5e8b14e5c2f8c038bb7c
inioluwa adebanjo <adeb6600@gmail.com>
2021-04-23 13:10:16 +0100

    refined corridor list

--- modules/static/index.js
+++ modules/static/index.js
@@ -87,7 +87,7 @@ module.exports[Symbol.for('plugin-meta')] = {
     return {
         message: corridors.ResponseMessage,
         count: corridors.Count,
-        corridors: refinedCorridors
+        corridors: corridors.Corridors;
     }
   }
   async function relationshipHandlers(req,reply) {
